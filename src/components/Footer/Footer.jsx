5321bb3147fef8a04a254b18ed527a6912fdf0b1
Wanchalerm Mitcachon <116241970+WanchalermMitcachon@users.noreply.github.com>
2024-06-22 18:07:37 +0700

    das
    dsa

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,29 +8,33 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <div
-      className="w-screen bg-[#33323D] flex flex-col items-center justify-center 
-    text-[#FFFFFF] py-14 mt-4 space-y-7
-     md:flex-row md:items-center md:justify-between md:space-x-16 md:px-10 md:space-y-0 lg:px-44"
+      className="w-screen bg-[#33323D] mx-auto
+     "
     >
-      <div className="md:flex md:space-x-8 md:items-center">
-        <img src={logoWhite} alt="logoWhite" />
-        <Link to="">
-          {" "}
-          <p className="hover:text-slightDesaturatedCyan">HOME</p>
-        </Link>
-        <Link to="portfolio">
-          {" "}
-          <p className="hover:text-slightDesaturatedCyan">PORTFOLIO</p>
-        </Link>
-        <Link to="contact">
-          {" "}
-          <p className="hover:text-slightDesaturatedCyan">CONTACT ME</p>
-        </Link>
-      </div>
-      <div className="flex space-x-4 py-2 md:space-x-6 md:items-center md:py-0">
-        <img src={githubWhite} alt="" className="cursor-pointer" />
-        <img src={linkinWhite} alt="" className="cursor-pointer" />
-        <img src={twitterWhite} alt="" className="cursor-pointer" />
+      <div className="py-14 md:flex md:justify-between md:max-w-[1050px] md:px-8 md:mx-auto  ">
+        <div
+          className="flex flex-col items-center justify-center 
+    text-[#FFFFFF] mb-10 mt-4 space-y-7  md:flex-row md:space-x-8 md:items-center"
+        >
+          <img src={logoWhite} alt="logoWhite" />
+          <Link to="">
+            {" "}
+            <p className="hover:text-slightDesaturatedCyan">HOME</p>
+          </Link>
+          <Link to="portfolio">
+            {" "}
+            <p className="hover:text-slightDesaturatedCyan">PORTFOLIO</p>
+          </Link>
+          <Link to="contact">
+            {" "}
+            <p className="hover:text-slightDesaturatedCyan">CONTACT ME</p>
+          </Link>
+        </div>
+        <div className="flex justify-center pb-8  space-x-4 py-2 md:space-x-6 md:items-center md:py-0">
+          <img src={githubWhite} alt="" className="cursor-pointer" />
+          <img src={linkinWhite} alt="" className="cursor-pointer" />
+          <img src={twitterWhite} alt="" className="cursor-pointer" />
+        </div>
       </div>
     </div>
   );
